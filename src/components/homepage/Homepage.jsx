a6dfed839f8d93a0de044028ef266d0e9ccefadd
itssameer <mesameer191@gmail.com>
2021-01-21 22:56:55 +0530

    homepage with minimum styles are ready

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Homepage.scss";
+
+function Homepage() {
+  return (
+    <div classname="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="description">Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="description">Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="description">Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="description">Shop now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
