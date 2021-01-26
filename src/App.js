03f5cd1ab242391cb69c56a722c5df0027fa0018
itssameer <mesameer191@gmail.com>
2021-01-26 13:34:39 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import ShopPage from "./pages/ShopPage/ShopPage";
 
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import HeaderComponent from "./components/header component/HeaderComponent";
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={ShopPage} />       
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
